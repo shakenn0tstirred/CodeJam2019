2914ae997a99dd48688c03b910beee6732d5a5ee
Destiny <32584522+shakenn0tstirred@users.noreply.github.com>
2020-04-11 00:18:56 -0400

    Update YouCanGoYourOwnWay
    This passed all three trials.

--- Qualification Round/YouCanGoYourOwnWay.py
+++ Qualification Round/YouCanGoYourOwnWay.py
@@ -1,1 +1,14 @@
+T = int(input()) # Number of cases
 
+for i in range(T):
+    N = int(input()) # Maze dimensions
+    P = input() # Lydia's valid maze path
+    y = [] # This will be the program's valid path
+    
+    for j in P: # The program will "mirror" Lydia's path
+        if j == 'E':
+            y.append('S')
+        else:
+            y.append('E')
+    
+    print("Case #" + str(i+1) + ": " + "".join(y))
